766ccb9396736eec81fd75eca7eaba3b14edfeaf
pm85 <pjm85@t-online.de>
2023-10-16 16:09:54 +0700

    Redux exercise

--- react-redux-master/src/actions/index.js
+++ react-redux-master/src/actions/index.js
@@ -1,1 +1,9 @@
-//actions
+// Function to create the increment action.
+const increment = (val) => {
+    return {
+        type : 'INCREMENT',
+        inc : val
+    }
+}
+
+export default increment;
--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
-import {Provider} from 'react-redux'
-import myReducers from './reducers'
+import {Provider} from 'react-redux';
+import myReducers from './reducers';
+import {legacy_createStore as createStore} from 'redux';
 
-
-//Create the store
+// Create the store
 const myStore = createStore(myReducers);
 
-//This will console log the current state everytime the state changes
+// This will console log the current state everytime the state changes
 myStore.subscribe(()=>console.log(myStore.getState()));
 
-//Enveloping the App inside the Provider, ensures that the states in the store are available
-//throughout the application
-ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
+// Enveloping the App inside the Provider, ensures that the states in the store
+// are available throughout the application.
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={myStore}>
+        <App/>
+    </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
